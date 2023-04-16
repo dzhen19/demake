2d386b3ea61968bf27efdbf455efefcbb0ab50fa
Derrick <55254786+dzhen19@users.noreply.github.com>
2023-04-16 18:16:04 -0400

    updated from 8bitworkshop.com

--- demake.c
+++ demake.c
@@ -9,6 +9,9 @@
 #include "vrambuf.h"
 //#link "vrambuf.c"
 
+signed int time_limit= 7250;
+static unsigned char timer[3];
+//static char oam_id;
 byte actor_x[NUM_ACTORS];
 byte actor_y[NUM_ACTORS];
 byte actor_px[NUM_ACTORS];
@@ -19,9 +22,12 @@ byte holdable_y[NUM_ACTORS];
 sbyte actor_dx[NUM_ACTORS];
 sbyte actor_dy[NUM_ACTORS];
 
+
+static unsigned char meals_made;
 byte actor_dir[NUM_ACTORS];
 byte actor_playable[NUM_ACTORS];
 byte actor_holding[NUM_ACTORS];
+Pot pot[NUM_POTS];
 
 static unsigned char map[MAP_WDT * MAP_HGT];
 static unsigned char item_map[MAP_WDT * MAP_HGT];
@@ -67,69 +73,135 @@ bool tile_is_solid(char tile)
   for (i = 0; i < NUM_SOLIDS; i++)
   {
     if (solids[i] == tile)
-      return true;
+    return true;
   }
   return false;
 }
+
+void initialize_pots(){
+  pot[0].px = 0x04;
+  pot[0].py = 0x0D;
+  pot[0].frames = 0;
+  pot[0].timer = 0;
+ 
+  pot[1].px = 0x0B;
+  pot[1].py = 0x0D;
+  pot[1].frames = 0;
+  pot[1].timer = 0;
+}
+void update_pot(unsigned char px, unsigned char py){
+  int ptr= MAP_ADR(px, py+2);
+  for (i = 0; i < NUM_POTS; i++)
+  {
+    if(pot[i].timer == 4) pot[i].isOn = false;
+    if(pot[i].isOn){
+      pot[i].frames += 1;
+      pot[i].timer = pot[i].frames/30;
+      oam_meta_spr_pal(pot[i].px*16, pot[i].py*16 +10, 0x00, boil_sequence[pot[i].timer]);
+    }
+  }
+  switch (px) // change behavior based on pot location
+  {
+    case 0x04: // pot 1
+    if(map[ptr] == POT && !pot[0].isOn && (actor_holding[0]== RICE || actor_holding[1]== RICE)){ //item_map[MAP_ADR(pot[0].px, pot[0].py+2)] == PREPPED_RICE
+      pot[0].isOn= true;
+      pot[0].frames = 0;
+      pot[0].timer = 0;
+    }
+    break;
+    case 0x0B: // pot 2
+    if(map[ptr] == POT && !pot[1].isOn && (actor_holding[0]== RICE || actor_holding[1]== RICE)){ // initializes pot
+      pot[1].isOn= true;
+      pot[1].frames = 0;
+      pot[1].timer = 0;
+    }
+    break;
+    default:
+    break;
+  }
+ 
+}
+
 
 void draw_holdable(int x, int y, int holdable_enum)
 {
   switch (holdable_enum)
   {
     oam_off = 0;
-  case FISH:
+    case FISH:
     oam_meta_spr_pal(x, y, 0x02, fish_spr);
     break;
-  case NORI:
+    case NORI:
     oam_meta_spr_pal(x, y, 0x03, nori_spr);
     break;
-  case RICE:
+    case RICE:
     oam_meta_spr_pal(x, y, 0x02, rice_spr);
     break;
-  case PREPPED_FISH:
+    case PREPPED_FISH:
     oam_meta_spr_pal(x, y, 0x02, prepped_fish_spr);
     break;
-  case PREPPED_RICE:
+    case PREPPED_RICE:
     oam_meta_spr_pal(x, y, 0x02, prepped_rice_spr);
     break;
-  case FISH_RICE:
+    case FISH_RICE:
     oam_meta_spr_pal(x, y, 0x02, rice_fish_spr);
     break;
-  case NORI_RICE:
+    case NORI_RICE:
     oam_meta_spr_pal(x, y, 0x03, nori_rice_spr);
     break;
-  case NORI_FISH:
+    case NORI_FISH:
     oam_meta_spr_pal(x, y, 0x01, nori_fish_spr);
     break;
-  case SUSHI:
+    case SUSHI:
     oam_meta_spr_pal(x, y, 0x01, sushi_spr);
     break;
-  default:
+    default:
     break;
+  }
+}
+
+void update_timer(){
+  timer[0] = time_limit/3600; // get minutes
+  timer[1] = (time_limit/60)%60/10; // tens of seconds
+  timer[2] = (time_limit/60)%10;
+}
+
+void draw_timer(){
+  if(time_limit>0){
+    oam_meta_spr_pal(116, 2, 0x02, count_sequence[timer[0]]);
+    oam_meta_spr_pal(124, 2, 0x02, count_sequence[timer[1]]);
+    oam_meta_spr_pal(132, 2, 0x02, count_sequence[timer[2]]);
+  }
+}
+void draw_score(){
+  oam_meta_spr_pal(116, 23, 0x01, score_sushi);
+  if(meals_made <10){
+    oam_meta_spr_pal(132, 15, 0x02, count_sequence[meals_made]);
   }
 }
 
 // main program
 void main()
 {
+  meals_made= 0;
   // draw preloaded nametable
   vram_adr(NAMETABLE_A);
   vram_write(nam, 1024);
   pal_all(PALETTE);
-
+ 
   vrambuf_clear();
   set_vram_update(updbuf);
-
+ 
   // now read vram and get map representation
   i16 = NTADR_A(0, 0);
   ptr = 0;
-
+ 
   for (i = 0; i < MAP_HGT; i++)
   {
     vram_adr(i16);
     vram_read(nameRow, 32);
     vram_adr(i16);
-
+   
     for (j = 0; j < MAP_WDT << 1; j += 2)
     {
       spr = nameRow[j];
@@ -137,26 +209,26 @@ void main()
       // item_map[ptr] = spr;
       switch (spr)
       { /*
-       case FISH_CRATE:
-         item_map[ptr] = FISH;
-         break;
-       case NORI_CRATE:
-         item_map[ptr] = NORI;
-         break;
-       case RICE_CRATE:
-         item_map[ptr] = RICE;
-         break;*/
-      default:
+        case FISH_CRATE:
+        item_map[ptr] = FISH;
+        break;
+        case NORI_CRATE:
+        item_map[ptr] = NORI;
+        break;
+        case RICE_CRATE:
+        item_map[ptr] = RICE;
+        break;*/
+        default:
         item_map[ptr] = NONE;
       }
-
+     
       ptr++;
     }
     i16 += 64;
   }
-
+ 
   ppu_on_all();
-
+  initialize_pots();
   for (p_i = 0; p_i < NUM_ACTORS; p_i++)
   {
     actor_x[p_i] = 120;
@@ -165,10 +237,10 @@ void main()
     holdable_x[p_i] = actor_x[p_i] - HDBL_OFFSET;
     holdable_y[p_i] = actor_y[p_i];
   }
-
+ 
   actor_x[1] = 100;
   actor_y[1] = 100;
-
+ 
   // loop forever
   while (true)
   {
@@ -177,17 +249,17 @@ void main()
     {
       pad_t = pad_trigger(p_i);
       pad = pad_state(p_i);
-
+     
       // for collision detection w/ map representation
       actor_px[p_i] = actor_x[p_i] >> TILE_SIZE_BIT;
       actor_py[p_i] = actor_y[p_i] >> TILE_SIZE_BIT;
-
+     
       // take input
       if (pad & PAD_LEFT && actor_x[p_i] > 0)
       {
         actor_dx[p_i] = -vel;
         actor_dir[p_i] = LEFT;
-
+       
         holdable_x[p_i] = actor_x[p_i] + HDBL_OFFSET;
         holdable_y[p_i] = actor_y[p_i];
       }
@@ -196,7 +268,7 @@ void main()
         actor_dx[p_i] = vel;
         actor_px[p_i]++;
         actor_dir[p_i] = RIGHT;
-
+       
         holdable_x[p_i] = actor_x[p_i] - HDBL_OFFSET;
         holdable_y[p_i] = actor_y[p_i];
       }
@@ -205,12 +277,12 @@ void main()
         actor_dx[p_i] = 0;
         actor_dir[p_i] = actor_dir[p_i];
       }
-
+     
       if (pad & PAD_UP && actor_y[p_i] > 0)
       {
         actor_dy[p_i] = -vel;
         actor_dir[p_i] = UP;
-
+       
         holdable_y[p_i] = actor_y[p_i] + HDBL_OFFSET;
         holdable_x[p_i] = actor_x[p_i];
       }
@@ -219,7 +291,7 @@ void main()
         actor_dy[p_i] = vel;
         actor_py[p_i]++;
         actor_dir[p_i] = DOWN;
-
+       
         holdable_y[p_i] = actor_y[p_i] - HDBL_OFFSET;
         holdable_x[p_i] = actor_x[p_i];
       }
@@ -228,11 +300,14 @@ void main()
         actor_dy[p_i] = 0;
         actor_dir[p_i] = actor_dir[p_i];
       }
-
+     
       // draw player and player item
       oam_meta_spr_pal(actor_x[p_i], actor_y[p_i], player_pal[p_i], player_spr);
       draw_holdable(holdable_x[p_i], holdable_y[p_i], actor_holding[p_i]);
-
+      update_pot(actor_px[p_i], actor_py[p_i]);
+      //update_pot(actor_px[p_i], actor_py[p_i], &pot[1]);
+      update_timer();
+      draw_timer();
       // draw selected tile
       if (!tile_is_solid(map[MAP_ADR(actor_px[p_i], actor_py[p_i] + 2)]))
       {
@@ -242,82 +317,92 @@ void main()
       else
       {
         oam_meta_spr_pal(actor_px[p_i] * 16, actor_py[p_i] * 16, 0x03, highlight_spr); // place playable tile
-
+       
         // pick up / set down, use pad_trigger instead of pad_state
         // possibly modify what item player is holding
         if (pad_t & PAD_A)
         {
-          
+         
           held = actor_holding[p_i];
           ptr = MAP_ADR(actor_px[p_i], actor_py[p_i] + 2);
           prev_item = item_map[ptr];
-
+         
           // default interaction: swap items with target
           actor_holding[p_i] = item_map[ptr];
           item_map[ptr] = held;
-
+         
           // spec interaction: cut fish
           if (map[ptr] == CUTTING_BOARD && item_map[ptr] == FISH)
-            item_map[ptr] = PREPPED_FISH;
-
+          item_map[ptr] = PREPPED_FISH;
+         
           // spec interaction: boil rice
           else if (map[ptr] == POT && item_map[ptr] == RICE)
+          item_map[ptr] = PREPPED_RICE;
+         
+          else if (prev_item == PREPPED_RICE && (pot[0].isOn && actor_px[p_i] == pot[0].px) || (pot[1].isOn && actor_px[p_i] == pot[1].px)){ // player cant get rice till cooked
             item_map[ptr] = PREPPED_RICE;
-
+            actor_holding[p_i] = NONE;
+          }
           // spec interaction: fish rice
           else if ((prev_item == PREPPED_RICE && held == PREPPED_FISH) ||
-                   (prev_item == PREPPED_FISH && held == PREPPED_RICE))
+          (prev_item == PREPPED_FISH && held == PREPPED_RICE))
           {
             actor_holding[p_i] = NONE;
             item_map[ptr] = FISH_RICE;
           }
-
+         
           else if ((prev_item == PREPPED_RICE && held == NORI) ||
-                   (prev_item == NORI && held == PREPPED_RICE))
+          (prev_item == NORI && held == PREPPED_RICE))
           {
             actor_holding[p_i] = NONE;
             item_map[ptr] = NORI_RICE;
           }
-
+         
           else if ((prev_item == PREPPED_FISH && held == NORI) ||
-                   (prev_item == NORI && held == PREPPED_FISH))
+          (prev_item == NORI && held == PREPPED_FISH))
           {
             actor_holding[p_i] = NONE;
             item_map[ptr] = NORI_FISH;
           }
-
+         
           else if ((prev_item == NORI_RICE && held == PREPPED_FISH) ||
-                   (prev_item == PREPPED_FISH && held == NORI_RICE) ||
-                   (prev_item == NORI && held == FISH_RICE) ||
-                   (prev_item == FISH_RICE && held == NORI) ||
-                   (prev_item == NORI_FISH && held == PREPPED_RICE) ||
-                   (prev_item == PREPPED_RICE && held == NORI_FISH))
+          (prev_item == PREPPED_FISH && held == NORI_RICE) ||
+          (prev_item == NORI && held == FISH_RICE) ||
+          (prev_item == FISH_RICE && held == NORI) ||
+          (prev_item == NORI_FISH && held == PREPPED_RICE) ||
+          (prev_item == PREPPED_RICE && held == NORI_FISH))
           {
             actor_holding[p_i] = NONE;
             item_map[ptr] = SUSHI;
           }
-
+          else if (prev_item == SUSHI && map[ptr] == SERVE) //served meal
+          {
+            actor_holding[p_i] = NONE;
+            item_map[ptr] = NONE;
+            meals_made +=1;
+          }
+         
           // spec interaction: grab ingredient from crate if no items on crate or player
           if (held == NONE && prev_item == NONE)
           {
             switch (map[ptr])
             {
-            case FISH_CRATE:
+              case FISH_CRATE:
               actor_holding[p_i] = FISH;
               break;
-            case NORI_CRATE:
+              case NORI_CRATE:
               actor_holding[p_i] = NORI;
               break;
-            case RICE_CRATE:
+              case RICE_CRATE:
               actor_holding[p_i] = RICE;
               break;
             }
           }
-          
+         
         }
       }
     }
-
+   
     // draw all loose items
     for (i = 0; i < MAP_HGT; i++)
     {
@@ -330,17 +415,21 @@ void main()
         }
       }
     }
-
-
+   
+   
     // debug
-    *(unsigned char *)0x00f0 = px;
-    *(unsigned char *)0x00f2 = py;
+    px;
+    py;
+    *(unsigned char *)0x00f0 = actor_px[0];
+    *(unsigned char *)0x00f2 = actor_py[0];
+    draw_score();
+   
     //*(unsigned char*)0x00ff = map[MAP_ADR(px, py)];
     *(unsigned char *)0x00ff = actor_holding[0];
     *(unsigned char *)0x00f1 = actor_dir[0];
     *(unsigned char *)0x00f4 = pad_state(0);
+    time_limit--;
     oam_hide_rest(oam_off);
-
     ppu_wait_nmi();
   }
 }
--- setup.c
+++ setup.c
@@ -9,6 +9,7 @@
 
 // constants
 #define NUM_ACTORS 2
+#define NUM_POTS 2
 #define TILE_SIZE 16
 #define NUM_SOLIDS 9
 #define NUM_CRATES 3
@@ -18,7 +19,7 @@
 
 // bit sizes
 #define TILE_SIZE_BIT 4
-#define FP_BITS	4
+#define FP_BITS 4
 #define MAP_WDT_BIT 4
 
 // sprite map
@@ -34,7 +35,7 @@
 #define SERVE 0x1E
 
 // macros
-#define MAP_ADR(x,y)	((((y)-2)<<MAP_WDT_BIT)|(x))
+#define MAP_ADR(x,y) ((((y)-2)<<MAP_WDT_BIT)|(x))
 #define DEF_METASPRITE_2x2(name,code,pal)\
 const unsigned char name[]={\
         0,      0,      (code)+0,   pal, \
@@ -43,22 +44,69 @@ const unsigned char name[]={\
         8,      8,      (code)+17,   pal, \
         128};
 
+#define DEF_METASPRITE_1x2(name,code,pal)\
+const unsigned char name[]={\
+        0,      0,      (code)+0,   pal, \
+        8,      0,      (code)+1,   pal, \
+        128};
+
+#define DEF_METASPRITE_1x1(name,code,pal)\
+const unsigned char name[]={\
+        0,      8,      (code)+1,   pal, \
+        128};
+
+DEF_METASPRITE_1x2(cooked_0, 0x50, 0)
+DEF_METASPRITE_1x2(cooked_1, 0x60, 0)
+DEF_METASPRITE_1x2(cooked_2, 0x70, 0)
+DEF_METASPRITE_1x2(cooked_3, 0x80, 0)
+DEF_METASPRITE_1x2(cooked_4, 0x90, 0)
+DEF_METASPRITE_1x2(score_sushi, 0x74, 0)
+
+DEF_METASPRITE_1x1(zero, 0xA8, 0);
+DEF_METASPRITE_1x1(one, 0x9F, 0);
+DEF_METASPRITE_1x1(two, 0xA0, 0);
+DEF_METASPRITE_1x1(three, 0xA1, 0);
+DEF_METASPRITE_1x1(four, 0xA2, 0);
+DEF_METASPRITE_1x1(five, 0xA3, 0);
+DEF_METASPRITE_1x1(six, 0xA4, 0);
+DEF_METASPRITE_1x1(seven, 0xA5, 0);
+DEF_METASPRITE_1x1(eight, 0xA6, 0);
+DEF_METASPRITE_1x1(nine, 0xA7, 0);
+
+const unsigned char* const boil_sequence[5] = {
+  cooked_0,cooked_1,cooked_2,cooked_3,cooked_4
+};
+
+const unsigned char* const count_sequence[10] = {
+  zero,one,two,three,four,five,six,seven,eight,nine
+};
+
+typedef struct Pot {
+  bool isOn; // is Pot boiling?
+  unsigned char px; // x coordinate on map
+  unsigned char py; // y coordinate on map
+  unsigned int frames; // frame count of boiling time
+  unsigned int timer; // timer display time
+} Pot;
+
 /*{pal:"nes",layout:"nes"}*/
-const char PALETTE[32] = { 
-  0x03,			// screen color
+const char PALETTE[32] = {
+  0x03, // screen color
 
-  0x27,0x11,0x12,0x00,	// background palette 0
-  0x1C,0x20,0x2C,0x00,	// background palette 1
-  0x00,0x10,0x20,0x00,	// background palette 2
-  0x06,0x16,0x26,0x00,	// background palette 3
+  0x27,0x11,0x12,0x00, // background palette 0
+  0x1C,0x20,0x2C,0x00, // background palette 1
+  0x00,0x10,0x20,0x00, // background palette 2
+  0x06,0x16,0x26,0x00, // background palette 3
 
-  0x08,0x01,0x16,0x00,	// sprite palette 0
-  0x30,0x09,0x16,0x00,	// sprite palette 1
-  0x20,0x11,0x16,0x00,	// sprite palette 2
-  0x30,0x2A,0x09	// sprite palette 3
+  0x08,0x01,0x16,0x00, // sprite palette 0
+  0x30,0x09,0x16,0x00, // sprite palette 1
+  0x20,0x11,0x16,0x00, // sprite palette 2
+  0x30,0x2A,0x09 // sprite palette 3
 };
 
 DEF_METASPRITE_2x2(player_spr, 0x36, 0)
+DEF_METASPRITE_2x2(player1_spr, 0x38, 0)
+DEF_METASPRITE_2x2(player2_spr, 0x3A, 0)
 DEF_METASPRITE_2x2(rice_spr, 0x32, 0)
 DEF_METASPRITE_2x2(fish_spr, 0x30, 0)
 DEF_METASPRITE_2x2(nori_spr, 0x34, 0)
@@ -70,8 +118,11 @@ DEF_METASPRITE_2x2(sushi_spr, 0x74, 0)
 DEF_METASPRITE_2x2(nori_rice_spr, 0x76, 0)
 DEF_METASPRITE_2x2(nori_fish_spr, 0x78, 0)
 
+const unsigned char* const run_sequence[3] = {
+  player_spr, player1_spr, player_spr
+};
 
-const unsigned char solids[NUM_SOLIDS] = { 
+const unsigned char solids[NUM_SOLIDS] = {
   BARRIER_1, BARRIER_2, POT,
   CUTTING_BOARD, SURFACE, RICE_CRATE, FISH_CRATE, NORI_CRATE, SERVE
 };
